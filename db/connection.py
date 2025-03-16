4c394a0a48df3db3714fcecf16c72ea54e5c4aca
Camila <milarodcar@gmail.com>
2025-03-16 20:03:01 +0100

    Modification in  creation.py file, I'm bringing the env information with dotenv

--- db/connection.py
+++ db/connection.py
@@ -1,15 +1,17 @@
 import os
+from dotenv import load_dotenv
 import psycopg2
-from config import DB_CONFIG
+
+load_dotenv()
 
 def connect_to_database():
     try:
         conn = psycopg2.connect(
-            user=DB_CONFIG["user"],
-            host=DB_CONFIG["host"],
-            port=DB_CONFIG["port"],
-            dbname=DB_CONFIG["dbname"],
-            password=DB_CONFIG["password"],
+            user=os.getenv("DB_USER"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            password=os.getenv("DB_PASSWORD")
         )
         print("Connection successful!")
         return conn
